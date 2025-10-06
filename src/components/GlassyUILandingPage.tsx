65574c067418ce53c81cab2152731dd263b52c8c
Nikita <avnixarma18@gmail.com>
2025-10-06 21:08:53 +0530

    hii

--- src/components/GlassyUILandingPage.tsx
+++ src/components/GlassyUILandingPage.tsx
@@ -27,7 +27,7 @@ const GlassyUILandingPage: React.FC = () => {
       <div className='relative z-10 w-full max-w-4xl'>
         <header className='w-full flex justify-between items-center mb-8'>
           <div className='text-2xl font-bold text-white'>
-            <span className='text-blue-400'>Priyansh </span>
+            <span className='text-blue-400'>Nikita Sharma</span>
           </div>
           <a
             href={githubRepoUrl}
